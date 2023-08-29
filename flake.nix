b1b88ef88f3f57c1a5593f0b3764522851418f41
waotzi <waotzi@proton.me>
2023-08-29 03:14:58 +0000

    add parcel and refactor code

--- flake.nix
+++ flake.nix
@@ -1,16 +1,12 @@
 {
   description = "My project with npm serve";
 
-  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-unstable";
+  inputs.nixpkgs.url = "github:NixOS/nixpkgs/nixos-23.05";
 
   outputs = { self, nixpkgs }: {
     defaultPackage.x86_64-linux = with nixpkgs.legacyPackages.x86_64-linux; stdenv.mkDerivation {
       name = "my-serve-project";
       buildInputs = [ nodejs ];
-      shellHook = ''
-        npm install serve
-        PATH=$PATH:$(pwd)/node_modules/.bin
-      '';
     };
   };
 }
