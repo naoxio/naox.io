ff116bd0293d0e0d35a03434a2f521e5f393043b
waotzi <waotzi@proton.me>
2023-09-03 06:02:12 +0000

    improve card display

--- assets/js/themeToggle.js
+++ assets/js/themeToggle.js
@@ -3,16 +3,24 @@ document.addEventListener("DOMContentLoaded", function() {
 
     // On page load, set the checkbox state from localStorage
     const themeState = localStorage.getItem("themeState");
-    if (themeState) {
-        themeToggle.checked = themeState === "dark";
-    }
 
+    if (themeState) {
+        themeToggle.checked = themeState === "light";
+    }
+    if (themeState === "light") {
+        document.body.classList.add('light-theme');
+    } else {
+        document.body.classList.remove('light-theme');
+    }
+      
     // Add event listener to the checkbox
     themeToggle.addEventListener("change", function() {
         if (this.checked) {
-            localStorage.setItem("themeState", "dark");
-        } else {
             localStorage.setItem("themeState", "light");
+            document.body.classList.add('light-theme');
+        } else {
+            localStorage.setItem("themeState", "dark");
+            document.body.classList.remove('light-theme');
         }
     });
 });
